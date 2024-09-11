2db6c70fdda09e9e0cef7314b073561612535e84
Vladyslav Pavlenko <vladyslavpavlen@gmail.com>
2024-09-11 22:28:21 +0300

    feat(lab1): correct newton's method

--- lab1/internal/methods/newton/newton.go
+++ lab1/internal/methods/newton/newton.go
@@ -12,25 +12,26 @@ type Solution struct {
 func Solve(prec float64) (s Solution) {
 	x := 50.0
 	s.Steps = [][]float64{}
+	n := calcN(prec)
 
-	for {
+	for i := 0; i < n; i++ {
 		xNext := x - f(x)/fPrime(x)
-		s.Steps = append(s.Steps, []float64{x, f(x), fPrime(x), xNext})
-
-		if math.Abs(xNext-x) < prec {
-			s.Root = xNext
-			return s
-		}
+		s.Steps = append(s.Steps, []float64{xNext, f(xNext)})
 		x = xNext
 	}
+
+	s.Root = s.Steps[len(s.Steps)-1][0]
+	return s
 }
 
-// f(x) = x^3 + x^2 - 4x - 4
+func calcN(prec float64) int {
+	return int(math.Log2(((math.Log(0.4/prec))/(math.Log(1/0.311)))+1) + 1)
+}
+
 func f(x float64) float64 {
 	return math.Pow(x, 3) + math.Pow(x, 2) - 4*x - 4
 }
 
-// f'(x) = 3x^2 + 2x - 4
 func fPrime(x float64) float64 {
 	return 3*math.Pow(x, 2) + 2*x - 4
 }
